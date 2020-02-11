5b1333fbdf9166bf1a77a64ad674f5e35d80fd48
Jason Tsorlinis <jasontsorlinis@192-168-1-105.tpgi.com.au>
2020-02-12 08:54:07 +1100

    removed unecessary xorshift lines

--- xorshift.go
+++ xorshift.go
@@ -12,17 +12,6 @@ func NewSource(seed int64) *XorShift64Star {
 	return &tmpxs
 }
 
-// Seed use the provvided seed value to init XorShift64Star internal state.
-func (x *XorShift64Star) Seed(seed int64) {
-	x.s = uint64(seed)
-}
-
-// Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
-func (x *XorShift64Star) Int63() int64 {
-	return int64(x.Uint64() & (1<<63 - 1))
-
-}
-
 // Uint64 returns the next pseudo random number generated, before start you must provvide one 64 unsigned bit seed.
 func (x *XorShift64Star) Uint64() uint64 {
 	r := x.s * uint64(2685821657736338717)
