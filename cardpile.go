d11d1e9606424e3923814e5afb9c667ba7ec72a6
jtsorlinis <jtsorlinis@gmail.com>
2021-04-07 17:34:21 +1000

    use pcg and nearly divisionless for shuffle

--- cardpile.go
+++ cardpile.go
@@ -4,14 +4,31 @@ import (
 	"time"
 )
 
-// var rnd = NewSource(time.Now().Unix())
-var seed = time.Now().Unix()
+var state uint64 = uint64(time.Now().Unix())
 
-func xorShift() uint32 {
-	seed ^= seed << 13
-	seed ^= seed >> 17
-	seed ^= seed << 5
-	return uint32(seed)
+// From https://www.pcg-random.org/download.html#minimal-c-implementation
+func pcg32() uint32 {
+	oldState := state
+	state = oldState*6364136223846793005 + 1
+	xorshifted := uint32(((oldState >> 18) ^ oldState) >> 27)
+	rot := uint32(oldState >> 59)
+	return (xorshifted >> rot) | (xorshifted << ((-rot) & 31))
+}
+
+// use nearly divisionless technique found here https://github.com/lemire/FastShuffleExperiments
+func pcg32Range(s uint32) uint32 {
+	x := pcg32()
+	m := uint64(x) * uint64(s)
+	l := uint32(m)
+	if l < s {
+		t := -s % s
+		for l < t {
+			x = pcg32()
+			m = uint64(x) * uint64(s)
+			l = uint32(m)
+		}
+	}
+	return uint32(m >> 32)
 }
 
 // CardPile class
@@ -45,7 +62,7 @@ func (c *CardPile) Print() string {
 func (c *CardPile) Shuffle() {
 	var i = uint32(len(c.MCards) - 1)
 	for ; i > 0; i-- {
-		j := xorShift() % (i + 1)
+		j := pcg32Range(i + 1)
 		c.MCards[i], c.MCards[j] = c.MCards[j], c.MCards[i]
 	}
 }
